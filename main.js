dcb4e4678adb84654b1971b74c8fdd5dd899b373
nikhilappasani <121653373+nikhilappasani@users.noreply.github.com>
2023-01-05 14:01:01 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -24,3 +24,27 @@ faqs.forEach(faq => {
         }
     })
 })
+
+
+// Nav Bar Show/Hide
+
+const menu = document.querySelector(".nav__menu");
+const openBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+openBtn.addEventListener('click', () => {
+    menu.style.display = "flex"
+    closeBtn.style.display= "inline-block" ;
+    openBtn.style.display= "none" ; 
+})
+
+
+//close nav menu
+
+const closeNav = () => {
+    menu.style.display = "none"
+    closeBtn.style.display= "none" ;
+    openBtn.style.display= "inline-block" ; 
+}
+
+closeBtn.addEventListener('click', closeNav)
